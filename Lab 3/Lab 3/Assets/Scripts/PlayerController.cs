9c5483ad1b1244c5851e97d4e4671ae12a5a4ff5
Alexandr-Pyslar <alexandr.pyslar@gmail.com>
2020-01-20 15:53:39 +0300

    add lessons

---
+++ Lab 3/Lab 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horInput;
+    private float verInput;
+    public float speed = 10f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horInput = Input.GetAxis("Horizontal");
+        verInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horInput, 0 , verInput) * Time.deltaTime * speed);
+    }
+}
