ad2b0309f1d9d90f100d45fbb9ec625f27f155eb
Alexandr-Pyslar <alexandr.pyslar@gmail.com>
2020-01-21 22:11:59 +0300

    Prototype 4.1
    add:
    PLayerController
    Focal Point

---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float horInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horInput * rotationSpeed * Time.deltaTime);
+    }
+}
