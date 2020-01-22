b6b2c368b6c7b199da8038a4adbadb254a95edb4
Alexandr-Pyslar <alexandr.pyslar@gmail.com>
2020-01-22 18:13:37 +0300

    completed lesson 4.3

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private GameObject focalPoint;
     public float speed = 5f;
+    public bool hasPowerup = false;
+    public float powerUpStrength = 15f;
+    public GameObject powerUpIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        rb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        //rb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+        rb.velocity = focalPoint.transform.forward * forwardInput * speed;
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            powerUpIndicator.gameObject.SetActive(true);
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCoutdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCoutdownRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        hasPowerup = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
     }
 }
