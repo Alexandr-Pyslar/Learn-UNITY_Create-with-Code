31663aa2acdfe87e66803edb6e2c084ac9c536fa
Alexandr-Pyslar <alexandr.pyslar@gmail.com>
2020-01-26 23:43:19 +0300

    comleted lesson 5
    Игра наподобие фрутниндзя

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; //textmesh pro
+using UnityEngine.SceneManagement; // restart scene
+using UnityEngine.UI; // restart button
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> targets;
+    public List<GameObject> targets; // замена обычного массива
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton;
+    public bool isGameActive;
+    private int score;
     private float spawnRate = 1f;
 
 
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+     
     }
 
 
@@ -21,13 +30,40 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
 
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
 
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestarGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true; // присвоить до корутинов
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
     }
 }
